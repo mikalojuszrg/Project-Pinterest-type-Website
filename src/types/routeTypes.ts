f5442f4923d512d704bf2c6a1d27ecb898084296
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-13 20:46:11 +0200

    routes updated

--- src/types/routeTypes.ts
+++ src/types/routeTypes.ts
@@ -1,4 +1,20 @@
-export type Route = {
+import { PropsWithChildren } from "react";
+
+type MainRoute = {
   path: "/" | "/:id" | "/examples";
   Component: () => JSX.Element;
+};
+
+type AuthRoute = {
+  path: "/";
+  Component: () => JSX.Element;
+};
+
+export type MainLayoutRoutes = {
+  Layout: (children: PropsWithChildren) => JSX.Element;
+  routes: MainRoute[];
+};
+export type AuthLayoutRoutes = {
+  Layout: (children: PropsWithChildren) => JSX.Element;
+  routes: AuthRoute[];
 };
