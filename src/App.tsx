2453a16abe1ff4ac10646c4eb77706d62213f7ee
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-13 20:44:06 +0200

    userContext created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Routes from "./routes/Routes";
+import { UserProvider } from "./contexts/UserContext";
 
 const App = () => {
-  return <Routes />;
+  return (
+    <UserProvider>
+      <Routes />
+    </UserProvider>
+  );
 };
 
 export default App;
