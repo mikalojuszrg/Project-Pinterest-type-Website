cf780906a0f77794530844661b87c251138c281a
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-14 20:14:18 +0200

    form with formik

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,23 @@
-import { Avatar, Box, Paper, TextField } from "@mui/material";
+import * as Yup from "yup";
+
+import { Avatar, Box, Button, Paper } from "@mui/material";
+import { Field, Form, Formik } from "formik";
 
 import LockIcon from "@mui/icons-material/Lock";
+import { TextField } from "formik-mui";
 import Typography from "@mui/material/Typography";
+import { User } from "../../types/user";
+import { UserContext } from "../../contexts/UserContext";
+import { loginFormInitialValues } from "./const";
+import { useContext } from "react";
 
 const Login = () => {
+  const { handleLogin } = useContext(UserContext);
+
+  const onSubmit = (user: User) => {
+    console.log(user);
+    handleLogin(user);
+  };
   return (
     <Paper elevation={24}>
       <Box p={2}>
@@ -15,14 +29,39 @@ const Login = () => {
         <Typography variant="h6" textAlign="center" mb={2}>
           Sign in
         </Typography>
-        <TextField label="Email Address" sx={{ mb: 2 }} fullWidth required />
-        <TextField
-          type="password"
-          label="Password"
-          sx={{ mb: 2 }}
-          fullWidth
-          required
-        />
+        <Formik initialValues={loginFormInitialValues} onSubmit={onSubmit}>
+          {({ isSubmitting }) => (
+            <Form>
+              <Field
+                component={TextField}
+                type="email"
+                name="email"
+                label="Email Address"
+                sx={{ mb: 2 }}
+                fullWidth
+                required
+              />
+              <Field
+                component={TextField}
+                type="password"
+                name="password"
+                label="Password"
+                sx={{ mb: 2 }}
+                fullWidth
+                required
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                disabled={isSubmitting}
+                fullWidth
+                sx={{ mb: 2 }}
+              >
+                Sign in
+              </Button>
+            </Form>
+          )}
+        </Formik>
       </Box>
     </Paper>
   );
