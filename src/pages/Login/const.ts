3778512ae17966dc81bbd7b13cdd2cb2a9b4417d
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-15 20:02:45 +0200

    email validation types

--- src/pages/Login/const.ts
+++ src/pages/Login/const.ts
@@ -1,6 +1,17 @@
+import * as Yup from "yup";
+
+import { emailValidation, requierdField } from "../../consts/validation";
+
 import { User } from "../../types/user";
 
 export const loginFormInitialValues: User = {
   email: "",
   password: "",
 };
+
+export const loginValidationSchema: Yup.ObjectSchema<User> = Yup.object().shape(
+  {
+    email: Yup.string().email(emailValidation).required(requierdField),
+    password: Yup.string().required(requierdField),
+  }
+);
