ea50623e8dfe6dbb2d9922f91bf2808372e8313e
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-15 20:02:31 +0200

    imageList from material ui added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,26 @@
+import { ImageList, ImageListItem } from "@mui/material";
+
 import { useImages } from "../../hooks/images";
 
 const Home = () => {
   const { data } = useImages();
   const images = data || [];
-
+  console.log(images);
   return (
-    <div>
-      {images.map((image) => (
-        <img key={image.id} src={image.previewURL} />
+    <ImageList variant="masonry" cols={4} gap={8}>
+      {images.map((item) => (
+        <ImageListItem key={item.previewURL}>
+          <img
+            src={`${item.webformatURL}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.webformatURL}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.id.toString()}
+            loading="lazy"
+            style={{ borderRadius: 5 }}
+          />
+        </ImageListItem>
       ))}
-    </div>
+         {" "}
+    </ImageList>
   );
 };
-
 export default Home;
