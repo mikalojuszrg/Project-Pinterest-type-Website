39490d5ffe299a9188b8cef85bbed740a7239652
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-16 11:55:17 +0200

    added infinite sroll params

--- src/api/images.ts
+++ src/api/images.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const URL = "https://pixabay.com/api";
 const KEY = "34412180-42360e656df8c51e07bc28475";
 
-export const fetchImages = async (): Promise<PixabayImage[]> => {
-  const response = await axios.get(URL, { params: { key: KEY } });
+export const fetchImages = async (page: number): Promise<PixabayImage[]> => {
+  const response = await axios.get(URL, { params: { key: KEY, page } });
   return response.data.hits;
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,26 +1,51 @@
 import { ImageList, ImageListItem } from "@mui/material";
+import { useEffect, useState } from "react";
 
+import InfiniteScroll from "react-infinite-scroll-component";
+import { PixabayImage } from "../../types/image";
 import { useImages } from "../../hooks/images";
 
 const Home = () => {
-  const { data } = useImages();
+  const [page, setPage] = useState(1);
+  const [items, setItems] = useState<PixabayImage[]>([]);
+
+  const { data } = useImages(page);
   const images = data || [];
-  console.log(images);
+
+  useEffect(() => {
+    setItems((prevItems) => [...prevItems, ...images]);
+  }, [images]);
+
+  const fetchData = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
   return (
-    <ImageList variant="masonry" cols={4} gap={8}>
-      {images.map((item) => (
-        <ImageListItem key={item.previewURL}>
-          <img
-            src={`${item.webformatURL}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.webformatURL}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.id.toString()}
-            loading="lazy"
-            style={{ borderRadius: 5 }}
-          />
-        </ImageListItem>
-      ))}
-         {" "}
-    </ImageList>
+    <InfiniteScroll
+      dataLength={images.length}
+      next={fetchData}
+      hasMore={true}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+    >
+      <ImageList variant="quilted" cols={4} gap={8}>
+        {items.map((item) => (
+          <ImageListItem key={item.previewURL}>
+            <img
+              src={`${item.webformatURL}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.webformatURL}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.id.toString()}
+              loading="lazy"
+              style={{ borderRadius: 5, minHeight: 40 }}
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </InfiniteScroll>
   );
 };
 export default Home;
